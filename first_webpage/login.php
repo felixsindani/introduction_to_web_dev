268b3d6d352a42b5e698b2a97612c01efd97b772
felixsindani <felixsindani72@gmail.com>
2023-07-26 21:08:22 +0300

    updated login feature

--- first_webpage/login.php
+++ first_webpage/login.php
@@ -124,8 +124,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Login">
             </div>
+            <p>Reset password <a href="reset.php">reset</a>.</p>
             <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-            <p>Reset password <a href="/">reset-password</a>.</p>
+            
         </form>
     </div>
 </body>
--- first_webpage/welcome.php
+++ first_webpage/welcome.php
@@ -20,8 +20,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </style>
 </head>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-    <p>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to FariFarm.</h1>
+    <p> 
         <a href="login.php" class="btn btn-warning">Login</a>
         <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
         <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
